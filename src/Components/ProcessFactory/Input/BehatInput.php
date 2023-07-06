108db64025cf4affeeeae155831b4a8ced7c7c75
Maximilian Graf Schimmelmann <max@schimmelmann.org>
2023-07-06 08:59:08 +0200

    Adapting Tests.

--- src/Components/ProcessFactory/Input/BehatInput.php
+++ src/Components/ProcessFactory/Input/BehatInput.php
@@ -11,12 +11,14 @@ final class BehatInput extends AbstractInput
     public function __construct(
         string $executorParameters = '',
         string $parameters = '',
-        string $workingDirectory = '/tmp'
+        string $workingDirectory = '/tmp',
+        int $timeout = 600
     ) {
         $this->setExecutor((new PhpExecutableFinder())->find() ?: null);
         $this->setExecutorParameters($executorParameters);
         $this->setCommand(BEHAT_BIN_PATH); /** @phpstan-ignore-line */
         $this->setParameters($parameters);
         $this->setDirectory($workingDirectory);
+        $this->setTimeout($timeout);
     }
 }
